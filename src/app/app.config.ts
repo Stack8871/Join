66cd91932466dcab7eb5629750d4f5e145265140
afk-Jane <j.domsgen@protonmail.com>
2025-06-20 22:16:27 +0200

    basic firebase-setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+import { firebaseConfig } from '../firebase.config';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage())
   ]
 };
---
+++ src/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRbRC6i5CNht1LxC8nz2kjypauT7lEHQ0",
+  authDomain: "join-678f7.firebaseapp.com",
+  projectId: "join-678f7",
+  storageBucket: "join-678f7.firebasestorage.app",
+  messagingSenderId: "464140951729",
+  appId: "1:464140951729:web:27908aab84611ff3268290"
+};
