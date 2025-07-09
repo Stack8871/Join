e7f2c56bb69eb1f61920514c02d7bf123b3386c5
Markus Fischer <fischer@mideasdesign.de>
2025-07-09 20:41:38 +0200

    move files from old repository

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ManageTask } from './board/manage-task/manage-task';
+import { summary } from './summary/summary';
+import { AddTask } from './board/add-task/add-task';
+import { TaskDetail } from './board/task-detail/task-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ManageTask},
+  { path: 'board', component: ManageTask },
+  { path: 'summary', component:summary},
+  { path: 'add-task', component: AddTask },
+  { path: '**', redirectTo: '' }
+];
