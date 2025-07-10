4d28e5e1da630a5e48ca37a4c1a4c35cedc29870
Marco Giering <marco.giering@icloud.com>
2025-07-10 14:43:33 +0200

    Original Contacts struktur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ManageTask } from './board/manage-task/manage-task';
 import { summary } from './summary/summary';
 import { AddTask } from './board/add-task/add-task';
+import { Contacts } from './contacts-sektion/contacts/contacts';
 import { TaskDetail } from './board/task-detail/task-detail';
 
 export const routes: Routes = [
@@ -9,5 +10,6 @@ export const routes: Routes = [
   { path: 'board', component: ManageTask },
   { path: 'summary', component:summary},
   { path: 'add-task', component: AddTask },
+  { path: 'contacts', component: Contacts },
   { path: '**', redirectTo: '' }
 ];
